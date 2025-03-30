38f1ecfb98d9d7e453d419fa03472c31faa188e9
evelynnguyenh <nhdieulinh06@gmail.com>
2025-03-30 13:52:57 -0500

    Update MyNodeLabels.java

--- src/myclassproject/mystorygraph/MyNodeLabels.java
+++ src/myclassproject/mystorygraph/MyNodeLabels.java
@@ -3,5 +3,5 @@ package myclassproject.mystorygraph;
 //Fill this enum with the labels you chose for the NODES of your story graph.
 public enum MyNodeLabels{
 	// Added by Tri Huynh, Hoang Dieu Linh Nguyen
-	root, BeginningScene, AcceptToGo, Consequence, RunAway, MagicForest, DelphineStudent, LearnLightMagic, LearnHealingMagic, LearnFireMagic, Rally, PopulistFaction, RileUpTheCrowd, PromisePopulistPower, PopulistRuleEnding, QuentinStudent, LearnDarkMagic, SoulbindingTrial, ForBiddenLibrary, TakeRevenge, Spare, Kill, CorruptionEnding, BecomeQueenEnding
+	root, BeginningScene, AcceptToGo, Consequence, RunAway, DelphineTalk, MagicForest, DelphineStudent, LearnLightMagic, LearnHealingMagic, LearnFireMagic, Rally, PopulistFaction, RileUpTheCrowd, PromisePopulistPower, PopulistRuleEnding, QuentinStudent, LearnDarkMagic, SoulbindingTrial, ForBiddenLibrary, TakeRevenge, Spare, Kill, CorruptionEnding, BecomeQueenEnding
 }
